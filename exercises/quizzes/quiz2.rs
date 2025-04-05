77f9dcd46c84940d4bee184a4af215b26d589bab
eaverdeja <eaverdeja@gmail.com>
2025-04-05 16:57:39 -0300

    2nd quiz

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -26,8 +26,29 @@ enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result = Vec::new();
+        for (i, command) in input {
+            let transformed = match command {
+                Command::Uppercase => i.to_uppercase(),
+                Command::Append(count) => i + &"bar".repeat(count),
+                Command::Trim => i.trim().to_string(),
+            };
+            result.push(transformed)
+        }
+        result
+    }
+
+    pub fn transformer_iter(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(i, command)| match command {
+                Command::Uppercase => i.to_uppercase(),
+                Command::Append(count) => i + &"bar".repeat(count),
+                Command::Trim => i.trim().to_string(),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -36,8 +57,9 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer_iter;
+
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
@@ -49,6 +71,27 @@ mod tests {
             ("bar".to_string(), Command::Append(5)),
         ];
         let output = transformer(input);
+
+        assert_eq!(
+            output,
+            [
+                "HELLO",
+                "all roads lead to rome!",
+                "foobar",
+                "barbarbarbarbarbar",
+            ]
+        );
+    }
+
+    #[test]
+    fn it_also_works_with_iterators() {
+        let input = vec![
+            ("hello".to_string(), Command::Uppercase),
+            (" all roads lead to rome! ".to_string(), Command::Trim),
+            ("foo".to_string(), Command::Append(1)),
+            ("bar".to_string(), Command::Append(5)),
+        ];
+        let output = transformer_iter(input);
 
         assert_eq!(
             output,
